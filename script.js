8d8e324e3ebf0fb5f2698c8b8028352987892704
Bas-sheikh <basilelshakhe@gmail.com>
2022-04-22 17:08:09 +0300

    create script by make for loop to add event click for all span in html page and make function to calc the result relates #4

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 // html script
+
+let result = document.querySelector(`.result`);
+let buttons = document.querySelectorAll(`span`);
+// ***********************************
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener(`click`, calc);
+}
+// ***********************************
+let count = 0;
+function calc (e) {
+
+  if (count === 1) {
+    result.textContent = `0`;
+    count = 0
+  }
+  if (    
+    (e.target.textContent.match(/[.]/)
+    && 
+    result.textContent.match(/[.]/))) {
+
+  }else if (
+    (e.target.textContent.match(/[*/+-]/)
+    && 
+    result.textContent[result.textContent.length - 1].match(/[*/+-]/))
+    ){
+      result.textContent = result.textContent.replace(result.textContent[result.textContent.length - 1],e.target.textContent)
+  }else if (e.target.textContent === `=`) {
+
+    console.log(typeof(result.textContent));
+    result.textContent = eval(result.textContent) || 0;
+    count++;
+
+  } else if (
+  (result.textContent === `0`) 
+  && (e.target.textContent !== `*`) 
+  && (e.target.textContent !== `/`)
+  ) {
+    result.textContent = e.target.textContent;
+  } else {
+    result.textContent += e.target.textContent;
+  }
+
+  if (e.target.textContent === `C`) {
+    result.textContent = `0`;
+  }
+}
